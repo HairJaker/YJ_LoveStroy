eac89cb6340bc0d90b7413dddb95435c6ed8a862
yujie <2662618853@qq.com>
2016-11-25 09:31:30 +0800

    设置根视图

--- YJ_LoveStroy/BaseViewController.m
+++ YJ_LoveStroy/BaseViewController.m
@@ -16,7 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setBackgroundColor];  // 设置渐变背景
+}
+
+#pragma mark ----  添加渐变背景色  -----
+
+-(void)setBackgroundColor
+{
+    CAGradientLayer *gradientLayer = [[CAGradientLayer alloc] init];
+    
+    gradientLayer.colors = @[(__bridge id)[UIColor blackColor].CGColor,(__bridge id)[UIColor colorWithRed:125/255.0f green:125/255.0f blue:125/255.0f alpha:1.0f].CGColor];
+    
+    //位置x,y    自己根据需求进行设置   使其从不同位置进行渐变
+    
+    gradientLayer.startPoint = CGPointMake(0, 1);
+    
+    gradientLayer.endPoint = CGPointMake(1, 0);
+    
+    gradientLayer.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+    
+    [self.view.layer addSublayer:gradientLayer];
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ YJ_LoveStroy/HomePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomePageViewController.h
+//  YJ_LoveStroy
+//
+//  Created by yujie on 16/11/24.
+//  Copyright © 2016年 yujie. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HomePageViewController : BaseViewController
+
+@end
