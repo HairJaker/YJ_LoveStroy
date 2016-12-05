ef35a6c765fdd97876106bdd8d22ebe7e10a909b
yujie <2662618853@qq.com>
2016-12-05 15:01:54 +0800

    collectionView

---
+++ YJ_LoveStroy/CustomCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.h
+//  YJ_LoveStroy
+//
+//  Created by yujie on 16/12/5.
+//  Copyright © 2016年 yujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionViewCell : UICollectionViewCell
+
+@end
